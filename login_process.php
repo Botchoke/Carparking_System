d373a46f4f5d6e4d38ee3ee04d1ea932bc5e8067
Aldiano Jullybert <aldianojullybert@gmail.com>
2026-07-06 17:34:25 +0800

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -1,50 +1,46 @@
 <?php
-
-
 session_start();
 include "db.php";
 
+if (isset($_POST['username']) && isset($_POST['password'])) {
 
-if(isset($_POST['username']) && isset($_POST['password'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
+    // Get the user by username
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = mysqli_query($conn, $sql);
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+    if (mysqli_num_rows($result) == 1) {
 
+        $row = mysqli_fetch_assoc($result);
 
-$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-$result = mysqli_query($conn,$sql);
+        // Verify the hashed password
+        if (password_verify($password, $row['password'])) {
 
+            $_SESSION['user'] = $username;
 
-if(mysqli_num_rows($result) == 1){
+            header("Location: index.php");
+            exit();
 
+        } else {
 
-$_SESSION['user'] = $username;
+            header("Location: login.php?error=1");
+            exit();
 
+        }
 
-header("Location: index.php");
-exit();
+    } else {
 
+        header("Location: login.php?error=1");
+        exit();
 
-}else{
+    }
 
+} else {
 
-header("Location: login.php?error=1");
-exit();
-
+    header("Location: login.php");
+    exit();
 
 }
-
-
-}else{
-
-
-header("Location: login.php");
-exit();
-
-
-}
-
-
 ?>
-
